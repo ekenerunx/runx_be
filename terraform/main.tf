28a74168f8a6b2f9cf66b3efc2b429b9fc2e5d4c
ekamanelly <ekamanelly@gmail.com>
2023-04-16 11:01:49 +0100

    _

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,14 @@
 provider "azurerm" {
   # ... other provider configuration ...
   features {}
-  client_id            = var.ARM_CLIENT_ID
-  client_secret        = var.ARM_CLIENT_SECRET
-  tenant_id            = var.ARM_TENANT_ID
-  subscription_id      = var.ARM_SUBSCRIPTION_ID
+  client_id            = "f9e112f2-1762-48ff-ba9c-66273945a9ab"
+  client_secret        = "wqz8Q~xONmch9DhQIOwpfPRhp4G3u9X_4uT6hawR"
+  tenant_id            = "d4089321-fbce-468b-b16b-b3cc8e265717"
+  subscription_id      = "5524a9f3-2588-4778-b99c-618dd11ae068"
+#   export ARM_CLIENT_ID="f9e112f2-1762-48ff-ba9c-66273945a9ab"
+# export ARM_CLIENT_SECRET="wqz8Q~xONmch9DhQIOwpfPRhp4G3u9X_4uT6hawR"
+# export ARM_TENANT_ID="d4089321-fbce-468b-b16b-b3cc8e265717"
+# export ARM_SUBSCRIPTION_ID="5524a9f3-2588-4778-b99c-618dd11ae068"
   # resource_group_name  = "runx-rg"
   # storage_account_name = "runxstorage"
   # container_name       = "tfstate"
