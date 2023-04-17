18c632cd3eefa38a645a533f37bd87fcad7d33f4
ekamanelly <ekamanelly@gmail.com>
2023-04-17 09:46:53 +0100

    create a new storage and rbac

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,9 +7,13 @@ terraform {
     }
   }
     backend "azurerm" {
-    resource_group_name  = "runx-rg"
-    storage_account_name = "runxstorage"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "sahelloazuretf123"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+    # resource_group_name  = "runx-rg"
+    # storage_account_name = "runxstorage"
+    # container_name       = "tfstate"
+    # key                  = "prod.terraform.tfstate"
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@
 provider "azurerm" {
   # ... other provider configuration ...
   features {}
-  client_id            = "f9e112f2-1762-48ff-ba9c-66273945a9ab"
-  client_secret        = "wqz8Q~xONmch9DhQIOwpfPRhp4G3u9X_4uT6hawR"
-  tenant_id            = "d4089321-fbce-468b-b16b-b3cc8e265717"
-  subscription_id      = "5524a9f3-2588-4778-b99c-618dd11ae068"
+  # client_id            = "f9e112f2-1762-48ff-ba9c-66273945a9ab"
+  # client_secret        = "wqz8Q~xONmch9DhQIOwpfPRhp4G3u9X_4uT6hawR"
+  # tenant_id            = "d4089321-fbce-468b-b16b-b3cc8e265717"
+  # subscription_id      = "5524a9f3-2588-4778-b99c-618dd11ae068"
 #   export ARM_CLIENT_ID="f9e112f2-1762-48ff-ba9c-66273945a9ab"
 # export ARM_CLIENT_SECRET="wqz8Q~xONmch9DhQIOwpfPRhp4G3u9X_4uT6hawR"
 # export ARM_TENANT_ID="d4089321-fbce-468b-b16b-b3cc8e265717"
@@ -17,8 +17,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "aci" {
-  name     = "aci-resource-group"
-  location = var.location
+  # name     = "aci-resource-group"
+  name = "rg-hello-azure-tf"
+  # location = var.location
+  location = "northcentralus"
 }
 
 
