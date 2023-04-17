60ce02d970181b6e828cabfae1a492993b38368d
ekamanelly <ekamanelly@gmail.com>
2023-04-17 11:56:50 +0100

    used data for resource group

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "aci" {
 
 resource "azurerm_subnet" "aci" {
   name                 = "aci-subnet"
-  resource_group_name  = azurerm_resource_group.aci.name
+  resource_group_name  = data.azurerm_resource_group.aci.name
   virtual_network_name = azurerm_virtual_network.aci.name
   address_prefixes     = ["10.0.1.0/24"]
 }
